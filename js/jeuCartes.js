a96b4e8f1f0abc99acaa0481c27816a00ea936e2
augiraudie <augustin.giraudier@etu.uca.fr>
2022-05-28 23:07:10 +0200

    ajout jeu de cartes retournées + restructuration

---
+++ js/jeuCartes.js
@@ -0,0 +1,72 @@
+
+var EndGame = false;
+
+function startGame(nbrCartes){
+    nbrCartes = parseInt(nbrCartes);
+
+    // Gestion d'erreur
+    if(nbrCartes < 2 || nbrCartes > 5){
+        console.log("erreur d'input dans la fonction de jeu...");
+        return;
+    }
+
+    // Lancement du jeu :
+
+    document.getElementById("page_1").classList.add("hide");
+    document.getElementById("page_2").classList.remove("hide");
+
+
+    let kickCard = getRandom(0,nbrCartes);
+
+    for(let i=0; i<nbrCartes; i++){
+        if(i == kickCard)
+            addCardTo("cards", true);
+        else
+            addCardTo("cards", false);
+    }
+
+}
+
+
+function addCardTo(pannelId, kick){
+
+    let div = document.getElementById(pannelId);
+    let divAdd = document.createElement("div")
+    divAdd.className = "carte";
+    divAdd.onclick = function(){CardClick(divAdd);};
+    div.appendChild(divAdd);
+
+    if(kick)
+        divAdd.innerHTML = `
+            <div class="carte-inner">
+            <div class="carte-front">
+                <img class="grow" src="./ressources/carte-kick.png" alt="">
+            </div>
+            <div class="carte-back">
+                <img class="grow" src="./ressources/carte-back.png" alt="">
+            </div>
+            </div>
+        `
+    else
+        divAdd.innerHTML = `
+        <div class="carte-inner">
+        <div class="carte-front">
+            <img class="grow" src="./ressources/carte-pasKick.png" alt="">
+        </div>
+        <div class="carte-back">
+            <img class="grow" src="./ressources/carte-back.png" alt="">
+        </div>
+        </div>
+        `
+
+}
+
+function getRandom(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
+
+function CardClick(div){
+    if(!EndGame)
+        div.classList.add("carte-return");
+    EndGame = true;
+}
---
+++ js/tools.js
@@ -0,0 +1,6 @@
+function PageLoaded(){
+    console.log("Game Loaded : Kick ou pas Kick v0.1\n Author : Augustin Giraudier");
+}
+function ReloadPage(path="./index.html"){
+    window.location = path;
+}
